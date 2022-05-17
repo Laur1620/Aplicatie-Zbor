82958e8e853eb3c13c711437dd158b604e8c5c3a
Karasin Kristian <karasinkristian@gmail.com>
2022-05-17 22:26:20 +0300

    AZ-10 added flight service with database

---
+++ src/main/java/org/loose/fis/sre/services/FlightService.java
@@ -0,0 +1,31 @@
+package org.loose.fis.sre.services;
+
+import org.dizitart.no2.Nitrite;
+import org.dizitart.no2.objects.ObjectRepository;
+import org.loose.fis.sre.model.Flight;
+import org.loose.fis.sre.model.User;
+
+import java.util.ArrayList;
+
+import static org.loose.fis.sre.services.FileSystemService.getPathToFile;
+
+public class FlightService {
+    private static ObjectRepository<Flight> flightRepository;
+
+    public static void initDatabase() {
+        Nitrite database = Nitrite.builder()
+                .filePath(getPathToFile("flight.db").toFile()).openOrCreate();
+        flightRepository = database.getRepository(Flight.class);
+    }
+    public static void add_flight(Flight f){
+        flightRepository.insert(f);
+    }
+    public static ArrayList<Flight> get_flights(){
+        ArrayList<Flight> lista=new ArrayList<>();
+        for (Flight f:flightRepository.find(
+        )){
+            lista.add(f);
+        }
+        return lista;
+    }
+}
