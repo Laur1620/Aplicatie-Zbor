4c0a9528990897228623fca5ad5eafb8ebcd9050
Karasin Kristian <karasinkristian@gmail.com>
2022-05-17 22:39:42 +0300

    AZ-10 added buttons for add; delete; edit flights

--- src/main/java/org/loose/fis/sre/controllers/MainController.java
+++ src/main/java/org/loose/fis/sre/controllers/MainController.java
@@ -1,8 +1,10 @@
 package org.loose.fis.sre.controllers;
 
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import org.loose.fis.sre.Main;
 
 import java.io.IOException;
@@ -11,6 +13,28 @@ public class MainController {
     public void RegisterClick() throws IOException {
         Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("register.fxml"));
         Main.primaryStage.setScene(new Scene(root, 400, 300));
+    }
+
+    @FXML
+    private Button registerbtn;
+    @FXML
+    private Button loginbtn;
+    @FXML
+    private Button addbtn;
+    @FXML
+    private Button deletebtn;
+    @FXML
+    private Button editbtn;
+
+    public void initialize(){
+        if(Main.loggedIn){
+            registerbtn.setVisible(false);
+            loginbtn.setVisible(false);
+            addbtn.setVisible(true);
+            deletebtn.setVisible(true);
+            editbtn.setVisible(true);
+
+        }
     }
 
     public void LogInClick() throws IOException {
